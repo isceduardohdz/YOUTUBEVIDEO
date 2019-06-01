3c4b13abf8e1f3b68161a8e8a918b2d9aab1d3c3
Eduardo <tec.eduardohdz@outlook.com>
2019-05-31 23:04:22 -0500

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace YoutubeVideo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            wb_Video.Source = "https://www.youtube.com/embed/Pr_f6oaAZ_A";
+        }
+    }
+}
